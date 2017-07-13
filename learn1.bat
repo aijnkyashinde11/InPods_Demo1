f4ed8b2df8116fd1478bc1592cdd55e5dbe0fdfb
aijnkyashinde11 <aws.ajinkyas11@gmail.com>
2017-07-13 20:19:52 +0000

    .bat

---
+++ learn1.bat
@@ -0,0 +1,2 @@
+iisreset
+robocopy C:\inetpub\wwwroot\Orcas C:\inetpub\wwwroot\Backup\Orcas_Backup_%date:~-4,4%%date:~-10,2%%date:~7,2% /COPYALL /E
---
+++ learn2.bat
@@ -0,0 +1,9 @@
+robocopy C:\inetpub\wwwroot\Orcas C:\inetpub\wwwroot\Backup\Orcas_Backup_%date:~-4,4%%date:~-10,2%%date:~7,2% /COPYALL /E
+TIMEOUT 10
+xcopy /y C:\WebCofigAndFiles C:\inetpub\wwwroot\Orcas /s /e /k
+TIMEOUT 3
+cacls C:\inetpub\wwwroot\Orcas /t /e /g Everyone:f
+TIMEOUT 3
+RD /S /Q "C:\inetpub\wwwroot\Demo1"
+DEL /F /Q /A C:\inetpub\wwwroot\Demo1.sln
+iisreset
